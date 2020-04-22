17db527a5339e90de79026ca815f9e98c3ac5665
lex-na-at <atrujillo@na-at.com.mx>
2020-04-22 17:44:38 -0500

    add Propertiesin ConfigJpa

--- src/main/java/com/was/ConfigJpa.java
+++ src/main/java/com/was/ConfigJpa.java
@@ -1,9 +1,12 @@
 package com.was;
 
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,16 +21,30 @@ public class ConfigJpa {
 	@Autowired
 	private DataSource dataSource;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String hibernateDdlAuto;
+
+	@Value("${spring.jpa.properties.hibernate.dialect}")
+	private String dialect;
+
 	@Primary
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean emfBean = new LocalContainerEntityManagerFactoryBean();
 		emfBean.setPackagesToScan("com.*");
-		emfBean.setPersistenceUnitName("persistenceWas");
+		emfBean.setPersistenceUnitName("persistenceUnit");
 		emfBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 		emfBean.setDataSource(dataSource);
+		emfBean.setJpaProperties(additionalProperties());
+
 		return emfBean;
-		
+	}
+
+	Properties additionalProperties() {
+		Properties jpaProperties = new Properties();
+		jpaProperties.setProperty("hibernate.hbm2ddl.auto", hibernateDdlAuto);
+		jpaProperties.setProperty("hibernate.dialect", dialect);
+		return jpaProperties;
 	}
 
 }
